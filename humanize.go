b56c7c5903fd80df71e1d70d9f7b47e44ed4294d
Tim Heckman <t@heckman.io>
2018-12-05 00:29:40 -0800

    Update GoDoc to indicate package is deprecated
    Signed-off-by: Tim Heckman <t@heckman.io>

--- humanize.go
+++ humanize.go
@@ -1,3 +1,5 @@
+// Package humanize  shouldn't be used. If you'd like to humanize some data, check out
+// https://github.com/dustin/go-humanize instead.
 package humanize
 
 // Version is the SemVer 2.0 version for the package.
